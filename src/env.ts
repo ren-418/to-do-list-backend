204e8a2d03f0e89a03490f98b18a17b2aa4b8aa4
ren-418 <mrsteevtvw418@gmail.com>
2024-12-10 22:49:30 +0900

    fix: port err

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,7 @@ export const Env = {
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD || "",
-  port: Number(process.env.PORT) || 8080,
+  port: Number(process.env.PORT) || 10000,
   dbPort: Number(process.env.DB_PORT) || 3306,
   dbName: process.env.DB_NAME,
   secretKey: process.env.SECRET_KEY,
